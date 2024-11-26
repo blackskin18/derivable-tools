2572850bb9ae943e3d965bbf9bf11f4ad8b8bd25
athevinha <nguyenthevinh1002@gmail.com>
2024-11-26 14:20:24 +0700

    build

--- dist/services/balanceAndAllowance.js
+++ dist/services/balanceAndAllowance.js
@@ -268,6 +268,9 @@ class BnA {
         ]);
         Object.keys(uniPosV3Data).map(posKey => {
             uniPosV3Data[posKey].poolState = uniPoolV3Data[uniPosV3Data[posKey].poolAddress];
+            if (Number(uniPosV3Data[posKey].liquidity) === 0) {
+                delete uniPosV3Data[posKey];
+            }
         });
         return uniPosV3Data;
     }
